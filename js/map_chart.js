35b50e6091848980a0b74d9b2570e5c498d8607c
hugodsc <hugo.sousa.cardoso@tecnico.ulisboa.pt>
2016-11-28 18:32:32 +0000

    Tooltip added to map chart
    Tooltip added to map chart

--- js/map_chart.js
+++ js/map_chart.js
@@ -7,17 +7,28 @@ function map_chart(){
   if (arguments.length > 0){
       for (var i = 0; i < arguments.length ; i++){
       	console.log(arguments[i].data.country[0])
-	  	parsedCountryData.push({1800:"1", Country:arguments[i].data.country[1]});
+	  	parsedCountryData.push({1800:"1", Country:arguments[i].data.country[1], Game: arguments[i].data.name});
       }
   }
    var map = d3.geomap.choropleth()
                   .geofile('lib/d3-geomap/topojson/world/countries.json')
-                  .colors(['green','red'])
+                  .colors(['green','#7D26CD'])
                   .column('1800')
                   .domain([0, 1])
                   .legend(false)
-                  .unitId('Country');
-   d3.select('#map')
-        .datum(parsedCountryData)
-        .call(map.draw, map);
+                  .unitId('Country')
+                  .zoomFactor(6)
+                  .unitTitle(function(d){
+                  	var toReturn = "";
+                  	toReturn += d.properties.name + "\n"
+                  	for (var i = 0; i < parsedCountryData.length;i++){
+                  		if(d.id == parsedCountryData[i].Country)
+							toReturn += parsedCountryData[i].Game + "\n"
+                  	}
+                  	return toReturn
+                  }) ;
+    	          
+   		d3.select('#map')
+            .datum(parsedCountryData)
+    		.call(map.draw, map);
 }
